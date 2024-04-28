80b577de487e4c928a18aebcf3d9c9ce2d80d72b
Michal Ciberej <michal.ciberej@gmail.com>
2024-04-28 13:06:32 +0200

    Separate routes nad setup auth

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import express from 'express';
 import path from 'path';
+import passport from 'passport';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import cors from 'cors';
+import home from './routes/home';
+import register from './routes/register';
 
 require('dotenv').config();
 const app = express();
@@ -7,12 +13,28 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+app.use(
+  session({
+    secret: process.env.SECRET!,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: process.env.DATABASE_URL }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
+
+require('./config/auth');
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(home);
+app.use(register);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
