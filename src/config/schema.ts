1a1dd67f61a46eae32a70ec329345a887ee36760
Michal Ciberej <michal.ciberej@gmail.com>
2024-04-29 23:25:06 +0200

    Add relations, permision, global type declaration

--- src/config/schema.ts
+++ src/config/schema.ts
@@ -3,15 +3,20 @@ import mongoose, { InferSchemaType } from 'mongoose';
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  _id: { type: mongoose.Types.ObjectId, required: true },
-  username: { type: String, required: true },
+  _id: { type: Schema.ObjectId, required: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  messages: { type: [mongoose.Types.ObjectId], required: true },
+  permision: {
+    type: String,
+    default: 'member',
+    enum: ['admin', 'member'],
+  },
+  messages: [{ type: Schema.ObjectId, ref: 'Message' }],
 });
 
 const messageSchema = new Schema({
-  _id: { type: mongoose.Types.ObjectId, required: true },
-  postedBy: { type: mongoose.Types.ObjectId, required: true },
+  _id: { type: Schema.ObjectId, required: true },
+  postedBy: { type: Schema.ObjectId, required: true, ref: 'User' },
   postedOn: { type: Date, required: true },
   body: { type: String, required: true },
 });
@@ -21,3 +26,9 @@ export type MessageType = InferSchemaType<typeof messageSchema>;
 
 export const User = mongoose.model('User', userSchema);
 export const Message = mongoose.model('Message', messageSchema);
+
+declare global {
+  namespace Express {
+    interface User extends UserType {}
+  }
+}
